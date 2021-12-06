34d6bf94ac668c55622665a245dc63a011bc92cb
Subhadarsini Pattnaik <71827873+Subhadarsini-10@users.noreply.github.com>
2021-12-06 23:03:11 +0530

    wiring with button

---
+++ Banana-speak/app.js
@@ -0,0 +1,13 @@
+//having reference with button in html.
+
+var btnTranslate = document.querySelector("#btn-translate");
+
+var txtInput = document.querySelector("#txt-Input");
+console.log(txtInput);
+
+function clickHandler() {
+  console.log("clicked");
+  console.log("input", txtInput.value);
+}
+
+btnTranslate.addEventListener("click", clickHandler);
