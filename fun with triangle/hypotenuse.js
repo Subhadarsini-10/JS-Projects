57a9cba861ea20896734b72e81e4f81715291cfc
Subhadarsini Pattnaik <71827873+Subhadarsini-10@users.noreply.github.com>
2022-01-12 18:01:47 +0530

    Add files via upload

---
+++ fun with triangle/hypotenuse.js
@@ -0,0 +1,19 @@
+const sides = document.querySelectorAll(".side-input");
+const hypotenusebtn = document.querySelector("#hypotenuse-btn");
+const output = document.querySelector("#output");
+
+function calculateSumOfSquares(a, b) {
+  const sumOfSquares = a * a + b * b;
+  return sumOfSquares;
+}
+
+function calculateHypotenuse() {
+  const sumOfSquares = calculateSumOfSquares(
+    Number(sides[0].value),
+    Number(sides[1].value)
+  );
+  const lengthOfHypotenuse = Math.sqrt(sumOfSquares);
+  output.innerText = "The length of hypotenuse is " + lengthOfHypotenuse;
+}
+
+hypotenusebtn.addEventListener("click", calculateHypotenuse);
